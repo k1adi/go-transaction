6ec0f3e2f395e8be3c78a912a3c74887e644afce
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2023-10-26 20:34:18 +0700

    implement logger and session in server.go

--- delivery/server.go
+++ delivery/server.go
@@ -3,22 +3,33 @@ package delivery
 import (
 	"go-transaction/config"
 	"go-transaction/delivery/controller"
+	"go-transaction/delivery/middleware"
 	"go-transaction/manager"
 
 	"fmt"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 type appServer struct {
 	usecaseManager manager.UseCaseManager
 	engine         *gin.Engine
 	host           string
+	log            *logrus.Logger
 }
 
 func (a *appServer) initController() {
+	storeCookie := cookie.NewStore([]byte("secret"))
+
+	a.engine.Use(sessions.Sessions("session", storeCookie))
+	a.engine.Use(middleware.LoggerMiddleware(a.log))
 	controller.NewBankController(a.engine, a.usecaseManager.BankUsecase())
 	controller.NewMerchantController(a.engine, a.usecaseManager.MerchantUsecase())
+	controller.NewCustomerController(a.engine, a.usecaseManager.CustomerUsecase(), a.usecaseManager.AuthUsecase())
+	controller.NewAdminController(a.engine, a.usecaseManager.AdminUsecase(), a.usecaseManager.AuthUsecase())
 }
 
 func (a *appServer) Run() {
@@ -43,5 +54,6 @@ func Server() *appServer {
 		engine:         engine,
 		host:           host,
 		usecaseManager: useCaseManager,
+		log:            logrus.New(),
 	}
 }
